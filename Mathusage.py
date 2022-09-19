0728ef6aa753b1d1403099251c926eecf74237fb
ketan1623 <112724802+ketan1623@users.noreply.github.com>
2022-09-18 22:59:00 -0400

    my very first commit

---
+++ Mathusage.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a value: "))
+b=int(input("Enter b value: "))
+
+print("Add value",a+b)
+print("Sub value",a-b)
+print("Mul value",a*b)
+print("Div value",a/b)
+print("mod value",a%b)
+print("exp value",a**b)
